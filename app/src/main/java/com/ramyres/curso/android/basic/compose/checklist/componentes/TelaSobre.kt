57190761074d42d1a05c1a555fef1292be6e2e3d
yandorneles <yandorneles05@gmail.com>
2024-12-11 09:22:10 -0300

    Finalizando atividade 1

--- app/src/main/java/com/ramyres/curso/android/basic/compose/checklist/componentes/TelaSobre.kt
+++ app/src/main/java/com/ramyres/curso/android/basic/compose/checklist/componentes/TelaSobre.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
